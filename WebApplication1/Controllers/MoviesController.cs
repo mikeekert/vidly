41b3a355130cf88f6d733696a9bea8b980ff903b
michael ekert <mikeekert@users.noreply.github.com>
2018-05-31 23:25:24 -0500

    new new

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Id = 1, Name = "movie test"};
+            return View(movie);
+        }
+    }
+}
