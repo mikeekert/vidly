10461b3642fbc7baa2b1eedb4a624d60e69d1fec
michael ekert <mikeekert@users.noreply.github.com>
2018-06-01 22:02:24 -0500

    custom routes

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -8,7 +8,9 @@ namespace WebApplication1
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
+            routes.MapMvcAttributeRoutes();
+            
             routes.MapRoute(
                 "Default",
                 "{controller}/{action}/{id}",
--- WebApplication1/Controllers/MoviesController.cs
+++ WebApplication1/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -9,7 +10,31 @@ namespace WebApplication1.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Id = 1, Name = "movie test"};
-            return View(movie);
+//          return View(movie);
+//          return HttpNotFound();
+//          return new EmptyResult();
+            return RedirectToAction($"Index", $"Home", new {page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        } 
+        
+        // nav to movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
